a7ccff7d8376d7303beb37310afaf87fff28d69f
MausVII <valscatty@gmail.com>
2021-11-30 23:45:26 -0600

    Early Version of movie blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          highlight: '#00ADB5',
+          base: '#393E46'
+        }
+    },
+    
   },
   variants: {
     extend: {},
