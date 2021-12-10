d534ea816d893595288d1107629b53507ec3f44f
MausVII <valscatty@gmail.com>
2021-12-09 20:33:49 -0600

    Visual cleanup

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -29,7 +29,7 @@ const PostWidget = ({categories, slug}) => {
                     </div>
                     <div className='flex-grow ml-4'>
                         <p className='text-gray-500 font-xs'>
-                            {moment(post.createAt).format('MMM DD, YYYY')}
+                            {moment(post.createdAt).format('MMM DD, YYYY')}
                         </p>
                         <Link href={`/post/${post.slug}`} key={post.title}>
                             {post.title}
