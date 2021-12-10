38831555fb7b5713730e82384b87fe143e89635f
MausVII <valscatty@gmail.com>
2021-12-09 19:46:52 -0600

    more

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
-import { getPosts, getPostDetails } from '../../Services'
+import { getPosts, getPostDetails } from '../../services'
 
 const PostDetails = ({ post }) => {
     
